09d31ac0f34af51d19e4d5dc597b54da4fdce432
x-Kajanan-x <99195803+x-Kajanan-x@users.noreply.github.com>
2023-03-31 11:29:45 +0200

    Update Voiture.java

--- src/main/java/com/example/demo/data/Voiture.java
+++ src/main/java/com/example/demo/data/Voiture.java
@@ -15,8 +15,38 @@ public class Voiture {
         return id;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public void setId(int id) {
         this.id = id;
+    }
+    private String marque;
+
+    private double prix;
+
+    public Voiture() {}
+
+    public Voiture(String marque, double prix) {
+        this.marque = marque;
+        this.prix = prix;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
     }
 
 }
